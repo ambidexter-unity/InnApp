491f3e8d6cb21e121669bb384bad4becec2f58ae
Egor <orlegand@gmail.com>
2019-10-03 18:30:12 +0300

    _

---
+++ Assets/Plugins/InApp/IIAPController.cs
@@ -0,0 +1,14 @@
+﻿using Common.Service;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IAP
+{
+    public interface IIAPController : IGameService
+    {
+        void Buy(ProductNames idPurchase);
+
+        void AppleRestoreTransactions();
+    }
+}
---
+++ Assets/Plugins/InApp/IPersistentProduct.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Purchasing;
+
+namespace IAP
+{
+    public interface IPersistentProduct
+    {
+        bool ReadOnlyIsValid { get; }
+        ProductNames BaseID { get; }
+        ProductType ProductType { get; }
+        string ReadOnlyDescription { get; }
+        string ReadOnlyPrice { get; }
+        bool ReadOnlyIsBuy { get; }
+		int HoursCooldownPurchased { get; }
+		string ReadOnlyPurchase { get; }
+		Sprite ReadOnlyIcon { get; }
+        string ReadOnlyTitle { get; }
+    }
+}

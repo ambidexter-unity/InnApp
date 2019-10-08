cc31ff98c6004cacb0fec5db5254d482d408dfbf
GeorgAmisare <orlegand@gmail.com>
2019-10-08 16:33:03 +0300

    Работа над инициализацией продуктов

---
+++ test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    void Start()
+    {
+        SaveProductToPlayerPrefs();
+    }
+
+    private void SaveProductToPlayerPrefs(params string[] keys)
+    {
+        Debug.Log(keys.Length);
+    }
+}
